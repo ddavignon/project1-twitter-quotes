5cc4cdbff046008d692423e196e700e4654247ea
Dustin DAvignon <ddavignon@csumb.edu>
2017-01-28 04:44:23 +0000

    added sass file

--- README.md
+++ README.md
@@ -3,10 +3,18 @@
 - Dustin D'Avignon
 
 
-What was the theme you chose?
+--What was the theme you chose?
 
-How did you pick your searches to fit the theme?
+ :I chose cute puppies to keep the theme light.
 
-What are known problems, if any, with your project?
+--How did you pick your searches to fit the theme?
 
-How would you improve it if you had more time?
+ :Getting the theme to work with the twitter hashtags was easier than the images. I hade to use many filters for the image url in order to get a decent looking image.
+
+--What are known problems, if any, with your project?
+ 
+ :The Twitter API doesn't always return the best quotes. Sometimes the quotes are just advertisements.
+
+--How would you improve it if you had more time?
+
+ :I would use the streaming api to change the quote every few seconds.
--- app.py
+++ app.py
@@ -6,7 +6,8 @@ import random
 app = Flask(__name__)
 
 
-search_term = 'dapl';
+phrase = 'cute puppies'
+search_term = phrase.replace(' ', '%20').lower()
 
 
 def twitterApiData():
@@ -17,62 +18,60 @@ def twitterApiData():
     ACCESS_TOKEN = '800599760902045696-782ZftuV0oe8aCMZ2qbsmtB0GnB5c9K'
     ACCESS_SECRET = 'AY1y2y7NAqJzOXFOJZuZXL5HQQtNbSQPzVx8zkAXxULVF'
 
-        # # Create a HTTP connection pool manager
+    # Create a HTTP connection pool manager
     manager = urllib3.PoolManager()
-    
     # Set the variable to Twitter OAuth 2 endpoint
     oauth_url = 'https://api.twitter.com/oauth2/token' 
-    
     # Set the HTTP request headers, including consumer key and secret
     http_headers={'Authorization': "Basic %s" % base64.b64encode("%s:%s" % (CONSUMER_KEY,CONSUMER_SECRET)), 'Content-Type': 'application/x-www-form-urlencoded'} 
-    
     # Set the payload to the required OAuth grant type, in this case client credentials
     request_body="grant_type=client_credentials" 
-    
     # Send the request
     response = manager.urlopen("POST", oauth_url, headers=http_headers, body=request_body)
     
     # Read the response as JSON
     app_token = json.loads(response.data) 
-    
     # Set variable for search
-    url='https://api.twitter.com/1.1/search/tweets.json?q=%23' + search_term
-    
+    url='https://api.twitter.com/1.1/search/tweets.json?q=%23' + search_term + '&result_type=recent'
     # Set the Authorization header using the value of the access_token key from the app_token dictionary created above
     http_header={'Authorization': 'Bearer %s' % app_token ['access_token']}
-    
     # Send the request
     response = manager.urlopen('GET', url , headers=http_header) 
-    
-   #print json.loads(response.data)['statuses'][random.randint(0,len(json.loads(response.data)['statuses']) )]
         
-    return json.loads(response.data)['statuses'][random.randint(0,len(json.loads(response.data)['statuses']) )]
+    try:
+        tweet = json.loads(response.data)['statuses'][random.randint(0,len(json.loads(response.data)['statuses']) )]
+    except:
+        tweet = ""
+        
+    return tweet
 
 
 def gettyApiImage():
-        # api keys
+    
+    # api keys
     API_KEY = 'f9m9s47sgubqb62d3bnwt5qj'
 
     # # Create a HTTP connection pool manager
     manager = urllib3.PoolManager()
-    
     # Set variable for search
-    url='https://api.gettyimages.com/v3/search/images?phrase=' + search_term
-    
+    url='https://api.gettyimages.com:443/v3/search/images?fields=display_set&file_types=jpg&minimum_size=large&phrase=' + search_term
     # Set the Authorization header using the value of the access_token key from the app_token dictionary created above
     http_header={'Api-Key': API_KEY}
-    
     # Send the request
     response = manager.urlopen('GET', url , headers=http_header) 
     
     # Read the response, create dictionary and render HTML using data and template
-
-    return json.loads(response.data)['images'][random.randint(0, len(json.loads(response.data)['images']))]
+    try:
+        image = json.loads(response.data)['images'][random.randint(0, len(json.loads(response.data)['images']))]
+    except:
+        image = ""
+    
+    return image
 
 
 @app.route('/')
 def hello():
-    return render_template('index.html', token=twitterApiData(), img=gettyApiImage())
+    return render_template('index.html', tweet=twitterApiData(), img=gettyApiImage())
     
 
-app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
+app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)), debug=True)
